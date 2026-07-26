170e28f486671a801307e3d3cbe0364cdbddf696
smpn04 <smpn04duampanua@gmail.com>
2026-07-26 13:19:48 +0800

    Allow Vercel Blob remote images

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.public.blob.vercel-storage.com",
+        hostname: "*.public.blob.vercel-storage.com",
       },
     ],
   },
